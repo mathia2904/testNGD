625174f8a7b46f859748f31f9184b61305d42143
mathia2904 <gustum92@gmail.com>
2020-08-19 13:14:24 +0000

    Changed Info file

--- Info.java
+++ Info.java
@@ -3,3 +3,9 @@
 //git commit -m "message"
 //git push (all changes goes to GitHub server!)
 //git commit -am "message" (does add and commit at the same time!)
+
+
+//git branch (see branches)
+//git checkout -b "new branch" (create branch)
+//git checout "branch" (change branch)
+//git merge "branch" (merge "branch" into branch you are in (master))
